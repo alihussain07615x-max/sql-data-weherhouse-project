4e173ef58539d5845fd51060ef20b01e98a37c47
alihussain07615x-max <ali.hussain.07615x@gmail.com>
2026-05-14 15:08:01 +0300

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -18,7 +18,7 @@ Usage Example:
 */
 
 
-/* CREATE PROSEDDURE */
+/* CREATE PROCEDDURE */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
